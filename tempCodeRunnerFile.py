be148882020e1415f1870cbd1e3052df28b08569
Rushil <dubeyrushil9@gmail.com>
2025-05-27 14:58:47 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    car_left = cars.xcor() - 20   
-        car_right = cars.xcor() + 2
-        car_top = cars.ycor() + 10    
-        car_bottom = cars.ycor() - 10 
-        player_x = player.xcor()
-        player_y = player.ycor()
-        if car_left < player_x < car_right and car_bottom < player_y < car_top:
